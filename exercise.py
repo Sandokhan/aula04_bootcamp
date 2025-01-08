359c4ceefbb1dce4b9a99409d3b3c5f767841b7e
sandokhan <rhaydrick@gmail.com>
2025-01-08 21:50:10 +0000

    Solved exercise 2

--- exercise.py
+++ exercise.py
@@ -1,4 +1,13 @@
 # Exercise 1
-square_list : list = [x**2 for x in range(11)]
+# square_list : list = [x**2 for x in range(11)]
+# print(square_list)
 
-print(square_list)
+# Exercise 2
+language_list : list = ["Python", "Java", "JavaScript", "C++"]
+print(language_list)
+print("Removing C++...")
+language_list.remove("C++")
+print("Adding Ruby...")
+language_list.append("Ruby")
+print("Final list")
+print(language_list)
