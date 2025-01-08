aadc30b38599fe062764256121e2a72c53057bdd
sandokhan <rhaydrick@gmail.com>
2025-01-08 21:53:03 +0000

    Solved exercise 3

--- exercise.py
+++ exercise.py
@@ -3,11 +3,16 @@
 # print(square_list)
 
 # Exercise 2
-language_list : list = ["Python", "Java", "JavaScript", "C++"]
-print(language_list)
-print("Removing C++...")
-language_list.remove("C++")
-print("Adding Ruby...")
-language_list.append("Ruby")
-print("Final list")
-print(language_list)
+# language_list : list = ["Python", "Java", "JavaScript", "C++"]
+# print(language_list)
+# print("Removing C++...")
+# language_list.remove("C++")
+# print("Adding Ruby...")
+# language_list.append("Ruby")
+# print("Final list")
+# print(language_list)
+
+# Exercise 3
+bookshelf : dict = {'Title' : 'SPQR', 'Author' : 'Mary Beard', 'Year' : 2015}
+for key, value in bookshelf.items():
+    print(f"{key}: {value}")
