ae26fd2f92b592900dc909db29c51ef53bf18b43
sandokhan <rhaydrick@gmail.com>
2025-01-09 22:09:27 +0000

    Solved exercices 6 7 8

--- exercise.py
+++ exercise.py
@@ -26,12 +26,32 @@
 # print(count_letters(word))
 
 # Exercise 5
-fruit_list : list = ["maça", "banana", "cereja"]
-fruit_dict : dict = {"maça" : .45, "banana" : .3, "cereja" : .65}
-total : float = 0
+# fruit_list : list = ["maça", "banana", "cereja"]
+# fruit_dict : dict = {"maça" : .45, "banana" : .3, "cereja" : .65}
+# total : float = 0
 
-for k, v in fruit_dict.items():
-    if k in fruit_list:
-        total = total + v
+# for k, v in fruit_dict.items():
+#     if k in fruit_list:
+#         total = total + v
 
-print(total)
+# print(total)
+
+# Exercise 6
+# email : list = ["harry@potter.uk", "curtis@jobling.com", "harry@potter.uk", "jason@bourne.uk", "james@gunn.dc.us", "richard@darkins.com", "curtis@jobling.com"]
+# print(list(set(email)))
+
+# Exercise 7
+# ages : list = [10, 20, 30, 40, 50, 60, 12, 15, 18, 5, 6]
+# print([age for age in ages if age >= 18])
+
+# Exercise 8
+# people : list = [
+#     {"nome": "Alice", "idade": 30},
+#     {"nome": "James", "idade": 25},
+#     {"nome": "Carol", "idade": 20},
+#     {"nome": "Ash", "idade": 30},
+#     {"nome": "Bob", "idade": 32},
+# ]
+# people.sort(key=lambda people: people["nome"])
+
+# print(people)
